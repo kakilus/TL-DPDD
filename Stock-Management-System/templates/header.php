f00a2647e524d42376e2835dfc90b6f954056a78
kuba <24001453@student.stamford.ac.uk>
2025-10-21 09:47:23 +0100

    made a header that is included from all pages

--- Stock-Management-System/templates/header.php
+++ Stock-Management-System/templates/header.php
@@ -1,5 +1,21 @@
 <?php 
 include("../config.php");
+$pgName = basename($_SERVER['PHP_SELF']);
+if ($pgName == 'admin.php') {
+    $name = 'Admin Page';
+};
+if ($pgName == 'stockControl.php') {
+    $name = 'Stock Control';
+};
+if ($pgName == 'index.php') {
+    $name = 'Home';
+};
+if ($pgName == 'prototype.php') {
+    $name = 'Sign-In Page';
+};
+if ($pgName == 'signUp.php') {
+    $name = 'Sign-Up Page';
+};
 echo '<div class="logo"><img src="assets/images/clash-royale-emote.gif" alt="aaaaaa" class="img"></div>';
-echo "<header>" . $pageName . "</header>";
+echo "<header>" . $name . "</header>";
 ?>
